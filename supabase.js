600ecad15a5bf23ac0429fa5533e10c8d17c3335
rickwgarcia <rickwltrgarcia@gmail.com>
2026-04-09 11:17:46 -0600

    keys added

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // Called after the Supabase CDN script loads in each HTML file
 window._initSupabase = function() {
-  const SUPABASE_URL = 'https://YOUR_PROJECT_ID.supabase.co'
-  const SUPABASE_ANON_KEY = 'YOUR_ANON_PUBLIC_KEY'
+  const SUPABASE_URL = 'https://wvehsyufxgfefcfnidhu.supabase.co'
+  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2ZWhzeXVmeGdmZWZjZm5pZGh1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDE5OTUsImV4cCI6MjA5MTMxNzk5NX0.TTw2cIfB19Z02TgJ4Eh2-R49yauxAvWu3H5ZNXXQF6s'
   window._sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 }
