901647f3b4310e7192d11efe7533766cdb0f1fcb
rickwgarcia <rickwltrgarcia@gmail.com>
2026-08-09 13:59:24 -0600

    updated with actual quotes

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -4,31 +4,31 @@ export const testimonials: Testimonial[] = [
   {
     id: 'testimonial-rick-garcia',
     quote:
-      'The barrier to entry felt intimidating at first, but the Lobo VC Cup showed me how inviting and supportive this community really is.',
+      'Albuquerque entrepreneurship community is very inviting, supportive, and motivating.',
     name: 'Rick Garcia',
-    title: 'Startup Founder, Queue Skin',
+    title: 'Startup Track, Queue Skin',
     rating: 5,
   },
   {
     id: 'testimonial-loryn-montoya',
-    quote: 'I wanted to try something completely out of my comfort zone, and it turned into building a real company.',
+    quote: '...wanted to try something completely out of my comfort zone',
     name: 'Loryn Montoya',
-    title: 'Startup Founder, Queue Skin',
+    title: 'Startup Track, Queue Skin',
     rating: 5,
   },
   {
     id: 'testimonial-breanna-gutierrez',
     quote:
-      'I competed with no prior knowledge about the VC world, and walked away with an extensive amount of knowledge and the confidence to put myself out there.',
+      'I competed with no prior knowledge about the VC world, but after the tournament, I walked away with an extensive amount of knowledge',
     name: 'Breanna Gutierrez',
-    title: 'Venture Capital Track, G Ventures',
+    title: 'VC Track, G Ventures',
     rating: 5,
   },
   {
     id: 'testimonial-rocky-guerrero',
-    quote: 'The most rewarding part was the networking. I met so many people, including mentors, along the way.',
+    quote: 'The most rewarding aspect of the competition was the networking',
     name: 'Rocky Guerrero',
-    title: 'Venture Capital Track, G Ventures',
+    title: 'VC Track, G Ventures',
     rating: 5,
   },
 ];
