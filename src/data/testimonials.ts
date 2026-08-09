f481fd30d9e8cfa3de88dbdaeda272657fe812b5
rickwgarcia <rickwltrgarcia@gmail.com>
2026-08-09 13:54:36 -0600

    shortened quotes

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -4,15 +4,14 @@ export const testimonials: Testimonial[] = [
   {
     id: 'testimonial-rick-garcia',
     quote:
-      'I have been wanting to do a pitch competition since fall 2025; however, up until now, I found the barrier to entry quite intimidating. The Lobo VC Cup showed me that the UNM and Albuquerque entrepreneurship community is very inviting, supportive, and motivating.',
+      'The barrier to entry felt intimidating at first, but the Lobo VC Cup showed me how inviting and supportive this community really is.',
     name: 'Rick Garcia',
     title: 'Startup Founder, Queue Skin',
     rating: 5,
   },
   {
     id: 'testimonial-loryn-montoya',
-    quote:
-      'I chose to participate as a startup team because I had an idea for a company and also wanted to try something completely out of my comfort zone.',
+    quote: 'I wanted to try something completely out of my comfort zone, and it turned into building a real company.',
     name: 'Loryn Montoya',
     title: 'Startup Founder, Queue Skin',
     rating: 5,
@@ -20,15 +19,14 @@ export const testimonials: Testimonial[] = [
   {
     id: 'testimonial-breanna-gutierrez',
     quote:
-      'My biggest takeaway from this competition was to put myself out there more. I competed with no prior knowledge about the VC world, but after the tournament, I walked away with an extensive amount of knowledge.',
+      'I competed with no prior knowledge about the VC world, and walked away with an extensive amount of knowledge and the confidence to put myself out there.',
     name: 'Breanna Gutierrez',
     title: 'Venture Capital Track, G Ventures',
     rating: 5,
   },
   {
     id: 'testimonial-rocky-guerrero',
-    quote:
-      'The most rewarding aspect of the competition was the networking. I got to meet a lot of people through the event including some of the mentors on the VC side.',
+    quote: 'The most rewarding part was the networking. I met so many people, including mentors, along the way.',
     name: 'Rocky Guerrero',
     title: 'Venture Capital Track, G Ventures',
     rating: 5,
