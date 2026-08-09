fbea6b8276e47964483c73ca62b0fd4efa0a1c7c
rickwgarcia <rickwltrgarcia@gmail.com>
2026-08-09 14:21:20 -0600

    added details back

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,14 +6,14 @@ export const testimonials: Testimonial[] = [
     quote:
       'Albuquerque entrepreneurship community is very inviting, supportive, and motivating.',
     name: 'Rick Garcia',
-    title: '',
+    title: 'Startup Track',
     rating: 5,
   },
   {
     id: 'testimonial-loryn-montoya',
     quote: '...wanted to try something completely out of my comfort zone',
     name: 'Loryn Montoya',
-    title: '',
+    title: 'Startup Track',
     rating: 5,
   },
   {
@@ -21,14 +21,14 @@ export const testimonials: Testimonial[] = [
     quote:
       'I competed with no prior knowledge about the VC world, but after the tournament, I walked away with an extensive amount of knowledge',
     name: 'Breanna Gutierrez',
-    title: '',
+    title: 'VC Track',
     rating: 5,
   },
   {
     id: 'testimonial-rocky-guerrero',
     quote: 'The most rewarding aspect of the competition was the networking',
     name: 'Rocky Guerrero',
-    title: '',
+    title: 'VC Track',
     rating: 5,
   },
 ];
