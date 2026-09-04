af1932524c9e981fc211e255c6778756ca611d75
rickwgarcia <rickwltrgarcia@gmail.com>
2026-09-04 15:12:26 -0600

    reduce spacing

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -8,7 +8,7 @@ interface LogoProps {
 
 export default function Logo({ className = '', markClassName = '', textClassName = '' }: LogoProps) {
   return (
-    <a href="#top" className={`inline-flex items-center gap-3 ${className}`}>
+    <a href="#top" className={`inline-flex items-center gap-1 ${className}`}>
       <img src={logoMark} alt="" className={`h-[48px] w-[68px] ${markClassName}`} />
       <span className={`font-grotesk text-lg font-bold leading-none text-black sm:text-xl ${textClassName}`}>
         Lobo VC Cup
