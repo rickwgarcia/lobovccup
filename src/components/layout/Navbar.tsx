f11364c20afbc90a5ad0ddb9b6cdc756a988bb31
rickwgarcia <rickwltrgarcia@gmail.com>
2026-09-06 18:07:21 -0600

    new alignment v2

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,9 +10,9 @@ export default function Navbar() {
   return (
     <header id="top" className="sticky top-0 z-50 w-full bg-brand-silver">
       <Container className="flex items-center justify-between py-4 lg:h-[84px] lg:py-0">
-        <Logo className="-ml-5 sm:-ml-8 lg:-ml-16" />
+        <Logo />
 
-        <nav className="hidden items-center gap-8 md:flex md:-mr-8 lg:-mr-16">
+        <nav className="hidden items-center gap-8 md:flex">
           {navLinks.map((link) => (
             <a
               key={link.href}
